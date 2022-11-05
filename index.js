b42c2f8e7ec428ce85d671619aeae187c9d0f9c5
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-05 18:47:13 -0400

    commit tree 05746aa1b7fb138316583204e48b6e6be313783f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter(
+      (match) => match.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+
+  function fuzzyMatch(source, string) {
+    return source.filter(
+        (match) => match.toLowerCase().indexOf(string.toLowerCase()) === 0 
+
+    )
+  } 
+function matchName(source, sname){ 
+    return source.filter((record) => record.name === sname)
+}
